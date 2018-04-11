d52afc11c0260cea4768e284ff879d2f30dda95e
DerKnob <derrealknob@gmail.com>
2018-04-11 22:14:18 +0200

    added the correct BitClient.cs file to repo

--- BandsInTownSharp/Client/BitClient.cs
+++ BandsInTownSharp/Client/BitClient.cs
@@ -1,27 +1,85 @@
-﻿using RestSharp;
+using BandsInTownSharp.Exceptions;
+using BandsInTownSharp.ValueObject.Bit;
+using RestSharp;
 using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace BandsInTownSharp.Client
 {
     public partial class BitClient
     {
-        private const string baseUrl = "https://rest.bandsintown.com";
+        private const string restUrl = "https://rest.bandsintown.com";
 
-        private string apiKey;
         private RestClient restClient;
-
-        public int RequestCount { get; set; }
-        public long DataCount { get; set; }
+        private string apiKey;
 
         public BitClient(string apiKey)
         {
             this.apiKey = apiKey;
-            restClient = new RestClient(baseUrl);
+
+            restClient = new RestClient(restUrl);
             restClient.ClearHandlers();
+
             restClient.AddHandler("application/json", new JsonDeserializer());
-            //probly not needed...
-            RequestCount = 0;
-            DataCount = 0;
+        }
+
+        public string GetApiKey()
+        {
+            return apiKey;
+        }
+
+        public List<EventVo> GetArtistEvents(string artistname)
+        {
+            return GetArtistEvents(artistname, null, null);
+        }
+        
+        public List<EventVo> GetArtistEvents(string artistname, DateTime? startDate, DateTime? endDate)
+        {
+            RestRequest request = new RestRequest();
+            request.Resource = string.Format("artists/{0}/events", artistname);
+
+            request.AddParameter("app_id", apiKey);
+
+            // add search date range
+            if ((startDate.HasValue) && (endDate.HasValue))
+            {
+                StringBuilder temp = new StringBuilder();
+                temp.Append(startDate.Value.ToString("yyyy-MM-dd"));
+                temp.Append(",");
+                temp.Append(endDate.Value.ToString("yyyy-MM-dd"));
+
+                request.AddParameter("date", temp);
+            }
+
+            // make the reqeust and get response
+            IRestResponse<List<EventVo>> response = restClient.Execute<List<EventVo>>(request);
+
+            if (response.Data == null)
+            {
+                throw new BitException(response.ErrorMessage);
+            }
+
+            return response.Data;
+        }
+
+        public ArtistVo GetArtist(string artistname)
+        {
+            RestRequest request = new RestRequest();
+            request.Resource = string.Format("artists/{0}", artistname);
+
+            request.AddParameter("app_id", apiKey);
+
+            // make the reqeust and get response
+            IRestResponse<ArtistVo> response = restClient.Execute<ArtistVo>(request);
+
+            if (response.Data == null)
+            {
+                throw new BitException(response.ErrorMessage);
+            }
+
+            return response.Data;
         }
 
     }
